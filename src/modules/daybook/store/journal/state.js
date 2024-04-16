ecaacfab624b11fa65cacac66c8e12a26909f43e
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2024-04-16 12:14:19 -0400

    Adding to repository

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,17 @@
 export default () => ({
   isLoading: true,
-  entry: [1, 2, 3, 4],
+  entry: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ullam inventore neque quod excepturi tempora ipsa perspiciatis amet possimus, sed nesciunt? Quaerat obcaecati eaque unde! Fuga ipsum in provident illo!",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit voluptate sed molestiae aliquid nostrum error, rem beatae, adipisci eos dolore temporibus est minima similique blanditiis, corrupti alias ratione ipsum nemo?",
+      picture: null,
+    },
+  ],
 });

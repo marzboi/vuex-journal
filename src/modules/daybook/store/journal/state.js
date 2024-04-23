cf6e3d9760e11c36db5fd9058aeb6c01082921f4
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2024-04-23 16:46:42 -0400

    Set up firebase RESTapi

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -4,19 +4,19 @@ export default () => ({
     {
       id: "1",
       date: new Date().toDateString(),
-      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ullam inventore neque quod excepturi tempora ipsa perspiciatis amet possimus, sed nesciunt? Quaerat obcaecati eaque unde! Fuga ipsum in provident illo!",
+      text: "Lorem I, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ullam inventore neque quod excepturi tempora ipsa perspiciatis amet possimus, sed nesciunt? Quaerat obcaecati eaque unde! Fuga ipsum in provident illo!",
       picture: null,
     },
     {
       id: "2",
       date: new Date().toDateString(),
-      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit voluptate sed molestiae aliquid nostrum error, rem beatae, adipisci eos dolore temporibus est minima similique blanditiis, corrupti alias ratione ipsum nemo?",
+      text: "Lorem II ipsum, dolor sit amet consectetur adipisicing elit. Fugit voluptate sed molestiae aliquid nostrum error, rem beatae, adipisci eos dolore temporibus est minima similique blanditiis, corrupti alias ratione ipsum nemo?",
       picture: null,
     },
     {
       id: "3",
       date: new Date().toDateString(),
-      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit voluptate sed molestiae aliquid nostrum error, rem beatae, adipisci eos dolore temporibus est minima similique blanditiis, corrupti alias ratione ipsum nemo?",
+      text: "Lorem III ipsum, dolor sit amet consectetur adipisicing elit. Fugit voluptate sed molestiae aliquid nostrum error, rem beatae, adipisci eos dolore temporibus est minima similique blanditiis, corrupti alias ratione ipsum nemo?",
       picture: null,
     },
   ],
--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-success fs-3 fw-bold">{{ day }}</span>
-      <span class="mx-1 fs-3">{{ month }}</span>
-      <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-3">{{ month }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+      </div>
+
+      <div>
+        <button class="btn btn-danger mx-2">
+          Borrar
+          <i class="fa fa-trash-alt"></i>
+        </button>
+
+        <button class="btn btn-primary">
+          Subir Foto
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
 
-    <div>
-      <button class="btn btn-danger mx-2">
-        Borrar
-        <i class="fa fa-trash-alt"></i>
-      </button>
+    <hr />
 
-      <button class="btn btn-primary">
-        Subir Foto
-        <i class="fa fa-upload"></i>
-      </button>
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea v-model="entry.text" placeholder="Que sucedio hoy?"></textarea>
     </div>
-  </div>
 
-  <hr />
-
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea v-model="entry.text" placeholder="Que sucedio hoy?"></textarea>
-  </div>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/New_York_Comic_Con_2014_-_Dr._Robotnik_%2815314367147%29.jpg/800px-New_York_Comic_Con_2014_-_Dr._Robotnik_%2815314367147%29.jpg"
+      alt=""
+      class="img-thumbnail"
+    />
+  </template>
 
   <Fab icon="fa-save" />
-
-  <img
-    src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/New_York_Comic_Con_2014_-_Dr._Robotnik_%2815314367147%29.jpg/800px-New_York_Comic_Con_2014_-_Dr._Robotnik_%2815314367147%29.jpg"
-    alt=""
-    class="img-thumbnail"
-  />
 </template>
 
 <script>
@@ -77,7 +79,7 @@ export default {
     loadEntry() {
       const entry = this.getEntryById(this.id);
 
-      if (!entry) this.$router.push({ name: "no-entry" });
+      if (!entry) return this.$router.push({ name: "no-entry" });
 
       this.entry = entry;
     },
@@ -85,6 +87,12 @@ export default {
 
   created() {
     this.loadEntry();
+  },
+
+  watch: {
+    id() {
+      this.loadEntry();
+    },
   },
 };
 </script>
